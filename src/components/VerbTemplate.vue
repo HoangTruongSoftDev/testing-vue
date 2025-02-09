132c5f8fdf3f86035b6aa3951953cf8bbb85051b
HoangTruongSoftDev <hoangtruongpham73@gmail.com>
2025-02-09 13:37:07 -0500

    update

---
+++ src/components/VerbTemplate.vue
@@ -0,0 +1,46 @@
+<script setup>
+const verb = [
+  "j'abandonne",
+  "tu abandonnes",
+  "il/elle/on abandonne",
+  "nous abandonnons",
+  "vous abandonnez",
+  "ils/elles abandonnent",
+];
+</script>
+
+<template>
+  <div class="conjugation-container">
+    <h2>Present</h2>
+    <ul>
+      <li><strong>je</strong> vais</li>
+      <li><strong>tu</strong> vas</li>
+      <li><strong>il/elle/on</strong> va</li>
+      <li><strong>nous</strong> allons</li>
+      <li><strong>vous</strong> allez</li>
+      <li><strong>ils/elles</strong> vont</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.conjugation-container {
+  background-color: #d3eaff;
+  border: 1px solid #000;
+  padding: 50px;
+  text-align: center;
+}
+
+.conjugation-container h2 {
+  font-weight: bold;
+}
+
+.conjugation-container ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+.conjugation-container li {
+  margin: 5px 0;
+}
+</style>
